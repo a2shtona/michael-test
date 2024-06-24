07066d7d5276f6193d2b1e3c900dfe1994decb93
Abo <solbest205@gmail.com>
2024-06-24 01:07:32 -0400

    update App page

--- src/components/Pages/App.tsx
+++ src/components/Pages/App.tsx
@@ -6,20 +6,23 @@ const App = () => {
   return (
     <div className="bg-black w-full h-full py-2 pr-2">
       <div className="bg-card rounded-lg w-full h-full flex flex-col">
-        <div className="flex flex-row justify-between w-full border-b py-3.5 px-6 border-white/10">
+        <div className="flex flex-row justify-between w-full py-3.5 px-6 ">
           <div className="flex flex-row items-center gap-2">
             <FiSearch className="w-5 h-5 relative text-zinc-400" />
-            <span className="text-sm text-zinc-400 font-inter font-medium hidden sm:block">
-              {"What are you looking for?"}
-            </span>
+            <input
+              className="text-sm text-zinc-400 font-inter font-medium bg-transparent w-full focus:outline-none focus:border-none"
+              placeholder="What are you looking for?"
+              type="text"
+            />
           </div>
           <div className="px-4 py-1.5 bg-gradient-to-r from-[#C28851] to-[#B77536] border border-white/30 justify-center items-center gap-1 inline-flex rounded-full">
-          <BsStars className="w-5 h-5 relative text-neutral-200" />
-          <span className="text-sm text-neutral-200 font-inter font-medium hidden sm:block">
-            Create
-          </span>
+            <BsStars className="w-5 h-5 relative text-neutral-200" />
+            <span className="text-sm text-neutral-200 font-inter font-medium hidden sm:block">
+              Create
+            </span>
           </div>
         </div>
+        <div className="w-full h-[0.5px] bg-white/10"/>
         <div className="px-6 mt-8">
           <Carousel />
         </div>
